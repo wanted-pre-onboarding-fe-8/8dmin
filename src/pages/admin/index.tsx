fd344549d9dc2928c7a2214f76ae7a8b93aac1fb
salangdung-i <dladmswl92@naver.com>
2022-07-22 17:42:22 +0900

    feat: admin 페이지에 컴포넌트 조립

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import MenuBar from './menuBar';
 
-function Admin() {
-  return <div>Admin</div>;
+export default function Admin() {
+  return (
+    <Wrapper>
+      <Header>AI 학습용 교통 데이터 수집을 위한 클라우드 워커 지원 현황</Header>
+      <MenuBar />
+      {/* table */}
+    </Wrapper>
+  );
 }
 
-export default Admin;
+const Wrapper = styled.div`
+  padding: 10px 30px;
+`;
+
+const Header = styled.header`
+  font-weight: 700;
+  font-size: 24px;
+  margin-top: 30px;
+`;
