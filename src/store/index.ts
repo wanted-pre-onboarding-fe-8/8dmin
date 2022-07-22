3ed9b81a8d622564d5dd99ab3e51da402661e0f4
ohtaekwon <ohtk1119@gmail.com>
2022-07-22 16:30:32 +0900

    feat : 리코일 구현

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,24 @@
-export default '';
+import React from 'react';
+import { atom, selector, useRecoilValue } from 'recoil';
+
+export const applicantsState = atom({
+  key: 'applicantsState',
+  default: [],
+});
+
+export const firstApplicants = selector({
+  key: 'firstApplicants',
+  get: ({ get }) => {
+    const applicants = get(applicantsState);
+    console.log(applicants);
+    // const firstApply = applicants.filter;
+  },
+});
+
+// export const secondApplicants = selector({
+//   key : "secondApplicants",
+//   get: ({get})=>{
+//     const applicants = get(applicantsState);
+
+//   }
+// })
