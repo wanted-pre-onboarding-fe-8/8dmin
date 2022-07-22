f98d0772381465003511380177cc2de077853441
bebusl <bebus1998@naver.com>
2022-07-22 22:29:38 +0900

    feat: 약관 동의란 구현

--- src/pages/apply/Agree.tsx
+++ src/pages/apply/Agree.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { Checkbox, FormControlLabel } from '@mui/material';
+import React, { useEffect, useState, MouseEvent, useRef } from 'react';
+import { FormControlLabel, Checkbox } from '@mui/material';
 import { CheckCircleOutline, CheckCircle } from '@mui/icons-material';
+import { useForm, Controller } from 'react-hook-form';
+import { off } from 'process';
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
@@ -14,12 +15,78 @@ const Field = ({ description }: { description: string }) => {
   );
 };
 
+interface agreedState {
+  privacy: boolean;
+  info: boolean;
+}
+
 function Agree() {
+  const { register, watch, setValue } = useForm<agreedState>({
+    defaultValues: { privacy: false, info: false },
+  });
+  const [privacy, info] = watch(['privacy', 'info']);
+  const [isAllAgreed, setIsAllAgreed] = useState<boolean>(false);
+  const renderCount = useRef<number>(0);
+
+  useEffect(() => {
+    renderCount.current += 1;
+    console.log(renderCount.current);
+  });
+  useEffect(() => {
+    if (isAllAgreed) {
+      setValue('privacy', true);
+      setValue('info', true);
+    } else if (privacy && info) {
+      setValue('privacy', false);
+      setValue('info', false);
+    }
+  }, [isAllAgreed]);
+
+  useEffect(() => {
+    setIsAllAgreed(privacy && info);
+  }, [privacy, info]);
+
   return (
-    <div style={{ display: 'flex', flexDirection: 'column' }}>
-      <Field description='이용약관 모두 동의' />
-      <Field description='개인정보 처리방침 고지 (필수)' />
-      <Field description='제3자 정보제공 동의 (필수)' />
+    <div>
+      <FormControlLabel
+        onClick={(e) => {
+          e.preventDefault();
+          setIsAllAgreed((pre) => !pre);
+        }}
+        control={
+          <Checkbox
+            {...label}
+            checked={isAllAgreed}
+            icon={<CheckCircleOutline />}
+            checkedIcon={<CheckCircle />}
+          />
+        }
+        label={<p>이용약관 모두 동의</p>}
+      />
+      <FormControlLabel
+        {...register('privacy')}
+        control={
+          <Checkbox
+            checked={privacy}
+            {...label}
+            icon={<CheckCircleOutline />}
+            checkedIcon={<CheckCircle />}
+          />
+        }
+        label={<p>개인정보 처리방침 고지 (필수)</p>}
+      />
+      <FormControlLabel
+        {...register('info')}
+        control={
+          <Checkbox
+            checked={info}
+            {...label}
+            icon={<CheckCircleOutline />}
+            checkedIcon={<CheckCircle />}
+          />
+        }
+        label={<p>제3자 정보제공 동의 (필수)</p>}
+      />
     </div>
   );
 }
