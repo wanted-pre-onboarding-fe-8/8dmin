62475e300784a3ba07b88002174ce18ffcac7c40
ohtaekwon <ohtk1119@gmail.com>
2022-07-25 20:35:04 +0900

    feat: menuBar에 select전역 데이터 적용

--- src/pages/admin/menuBar.tsx
+++ src/pages/admin/menuBar.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '@mui/material/Button';
+<<<<<<< HEAD
 import Search from './search';
 import DownloadLink from '../../components/DownloadLink';
 
 export default function MenuBar() {
+=======
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { useRecoilState } from 'recoil';
+import { selectState } from '../../store';
+
+const selectOptions = ['지원날짜', '지원자명', '성별', '생년월일', '이용수단', '거주지'];
+
+export default function MenuBar() {
+  const [select, setSelect] = React.useState('지원날짜');
+  const [selecState, setSelectState] = useRecoilState(selectState);
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setSelect(event.target.value);
+    setSelectState(event.target.value);
+  };
+
+>>>>>>> e2e904a (feat: menuBar에 select전역 데이터 적용)
   return (
     <>
       <Wrapper>
