44e8c4d8fe181d62e03ceec36e134805b82b5967
corgi-world <developer.msw@gmail.com>
2022-07-25 13:15:51 +0900

    new: 이름, 생년월일, 연락처, 이메일 validation 구현

---
+++ src/utils/input/validation.ts
@@ -0,0 +1,9 @@
+export const NAME_VALIDATION = /^[가-힣]+$/;
+
+export const DOB_VALIDATION =
+  /(19[0-9]{2}|20[0-1][0-9]|202[0-2]).(0[1-9]|1[012]).(0[1-9]|[12][0-9]|3[01])$/;
+
+export const PHONE_VALIDATION = /^01([0|1|6|7|8|9])([0-9]{4})([0-9]{4})$/;
+
+export const EMAIL_VALIDATION =
+  /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.com$/i;
