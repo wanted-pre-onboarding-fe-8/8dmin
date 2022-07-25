711a2481a4a2d749ad6e55cc2de3528dac982dc6
ohtaekwon <ohtk1119@gmail.com>
2022-07-25 14:09:07 +0900

    feat: 임시 목데이터 타입

---
+++ src/store/types.ts
@@ -0,0 +1,29 @@
+export type Gender = '남' | '여';
+export type Transportation =
+  | '버스'
+  | '지하철'
+  | '택시'
+  | 'KTX/기차'
+  | '도보'
+  | '자전거'
+  | '전동킥보드'
+  | '자가용';
+export type region = {
+  city: string;
+  district: string;
+};
+export interface MockCandidate {
+  id: number;
+  series: number;
+  appliedAt: string;
+  name: string;
+  gender: Gender;
+  birth: string;
+  phone: string;
+  email: string;
+  transportation: Transportation;
+  region: region;
+  accepted: boolean;
+}
+
+export type MockCandidates = MockCandidate[];
