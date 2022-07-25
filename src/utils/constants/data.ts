88242d699eb3cc472a4e74689a7f60a78143d6e9
ohtaekwon <ohtk1119@gmail.com>
2022-07-25 17:39:16 +0900

    feat: 상수폴더 생성

---
+++ src/utils/constants/data.ts
@@ -0,0 +1,18 @@
+export const SELECT_CONSTANTS = {
+  SELECT_DATE: '지원날짜',
+  SELECT_NAME: '지원자명',
+  SELECT_GENDER: '성별',
+  SELECT_BIRTH: '생년월일',
+  SELECT_TRANSPORTAION: '이용수단',
+  SELECT_REGION: '거주지',
+} as const;
+
+export const ORDER_CONSTANTS = {
+  ORDER_ID: 'id',
+  ORDER_NAME: 'name',
+  ORDER_APPLIED_AT: 'appliedAt',
+  ORDER_GENDER: 'gender',
+  ORDER_BIRTH: 'birth',
+  ORDER_EMAIL: 'email',
+  ORDER_REGION: 'region',
+} as const;
