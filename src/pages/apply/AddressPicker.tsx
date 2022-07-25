e0c042c2b199225e152fed482969523240cc09fc
corgi-world <developer.msw@gmail.com>
2022-07-25 21:58:17 +0900

    feat: 거주지역 선택 picker 구현

---
+++ src/pages/apply/AddressPicker.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from 'react';
+import { REGION, typeMainRegion } from '../../utils/region';
+import styled from 'styled-components';
+import Picker from './Picker';
+
+const ITEM_HEIGHT = 70;
+const ITEM_COUNT = 3;
+
+interface IAddressPicker {
+  handleAddressSelect: (address: string) => void;
+}
+
+export default function AddressPicker({ handleAddressSelect }: IAddressPicker) {
+  const [mainIndex, setMainIndex] = useState(1);
+  const [subIndex, setSubIndex] = useState(1);
+
+  const mainItems = Object.keys(REGION);
+  const subItems = getSubItems(mainIndex);
+
+  const itemClickHandler = (
+    itemIndex: number,
+    selectedIndex: number,
+    setFn: React.Dispatch<React.SetStateAction<number>>,
+  ) => {
+    const increase = (prev: number) => prev + 1;
+    const decrease = (prev: number) => prev - 1;
+
+    if (itemIndex === selectedIndex - 1) {
+      setFn(decrease);
+    } else if (itemIndex === selectedIndex + 1) {
+      setFn(increase);
+    }
+  };
+
+  const initializeSubIndex = (itemIndex: number) => {
+    const subItemCount = getSubItems(itemIndex).length;
+    const initialIndex = subItemCount < ITEM_COUNT ? 0 : 1;
+    setSubIndex(initialIndex);
+  };
+
+  const buttonClickHandler = () => {
+    const address = indexToString(mainIndex, subIndex);
+    handleAddressSelect(address);
+  };
+
+  return (
+    <Wrapper>
+      <WrapperTitle>거주지역 선택</WrapperTitle>
+      <Box>
+        <Picker
+          title={'시/도'}
+          items={mainItems}
+          currentIndex={mainIndex}
+          itemHeight={ITEM_HEIGHT}
+          itemCount={ITEM_COUNT}
+          itemClickHandler={(itemIndex) => {
+            itemClickHandler(itemIndex, mainIndex, setMainIndex);
+            initializeSubIndex(itemIndex);
+          }}
+        />
+        <Picker
+          title={'시/구/군'}
+          items={subItems}
+          currentIndex={subIndex}
+          itemHeight={ITEM_HEIGHT}
+          itemCount={ITEM_COUNT}
+          itemClickHandler={(itemIndex) => {
+            itemClickHandler(itemIndex, subIndex, setSubIndex);
+          }}
+        />
+      </Box>
+      <Button onClick={buttonClickHandler}>확인</Button>
+    </Wrapper>
+  );
+}
+
+function getSubItems(mainIndex: number) {
+  const mainItems = Object.keys(REGION);
+  const mainRegion = mainItems[mainIndex] as typeMainRegion;
+  return REGION[mainRegion];
+}
+
+function indexToString(mainIndex: number, subIndex: number) {
+  const mainItems = Object.keys(REGION);
+  const subItems = getSubItems(mainIndex);
+
+  return `${mainItems[mainIndex]} ${subItems[subIndex]}`;
+}
+
+const Wrapper = styled.div`
+  background-color: white;
+  width: 400px;
+  padding: 10px;
+`;
+
+const Title = styled.p`
+  font-weight: 600;
+  text-align: center;
+`;
+const WrapperTitle = styled(Title)`
+  font-size: 18px;
+  padding: 15px;
+`;
+
+const Box = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 30px;
+`;
