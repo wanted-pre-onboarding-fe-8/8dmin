5fe330af37c1a071492b97122e1d874d6e3c6000
ohtaekwon <ohtk1119@gmail.com>
2022-07-25 14:07:29 +0900

    feat: 페이지마다 10개씩 출력하기 적용

---
+++ src/store/pageNationSelector.ts
@@ -0,0 +1,13 @@
+import { RecoilValue, selectorFamily } from 'recoil';
+import { MockCandidates } from './types';
+
+const STANDARD_NUMBER = 10;
+export const pageNationSelector = selectorFamily({
+  key: 'pageNationSelector',
+  get:
+    ({ candidates, page }: { candidates: RecoilValue<MockCandidates>; page: number }) =>
+    ({ get }) => {
+      const candidateState = get(candidates);
+      return candidateState.slice(page * STANDARD_NUMBER - STANDARD_NUMBER, page * STANDARD_NUMBER);
+    },
+});
