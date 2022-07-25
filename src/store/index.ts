8b1ffe1d6bcf376842b4ee727030c219241d41a0
ohtaekwon <ohtk1119@gmail.com>
2022-07-25 13:20:29 +0900

    feat: store들 한곳으로 호출하도록 통합

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,17 @@
-import { ApplicantsState } from './ApplicantsState';
-import { FirstApplicantsState } from './FirstApplicantsState';
-import { SecondApplicantsState } from './SecondApplicantsState';
+import { allCandidateState, seriesState, keywordState, selectState, pageState } from './atoms';
+import { searchSelector } from './searchSelector';
+import { candidateSelector } from './candidateSelector';
+import { sortCandidateSelector } from './sortCandidateSelector';
+import { pageNationSelector } from './pageNationSelector';
 
-export { ApplicantsState, FirstApplicantsState, SecondApplicantsState };
+export {
+  allCandidateState,
+  keywordState,
+  seriesState,
+  selectState,
+  pageState,
+  candidateSelector,
+  sortCandidateSelector,
+  searchSelector,
+  pageNationSelector,
+};
