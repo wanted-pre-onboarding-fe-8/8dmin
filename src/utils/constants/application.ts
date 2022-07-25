0d0b9248139766205cd5db82e8e9574e17b7d1cd
salangdung-i <dladmswl92@naver.com>
2022-07-25 16:43:04 +0900

    feat: select option 상수, 정규식 상수

---
+++ src/utils/constants/application.ts
@@ -0,0 +1,10 @@
+export const SELECT_OPTIONS_CONTANST = [
+  { key: 'dateOfApply', value: '지원날짜' },
+  { key: 'name', value: '지원자명' },
+  { key: 'gender', value: '성별' },
+  { key: 'dateOfBirth', value: '생년월일' },
+  { key: 'transport', value: '이용수단' },
+  { key: 'address', value: '거주지' },
+];
+
+export const DATE_REGEX = /^[0-9\b]{0,8}$/;
