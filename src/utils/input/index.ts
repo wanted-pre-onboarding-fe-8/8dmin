2cf72b6ba7c67b3dd8f1650afc1b7570966c6ab6
corgi-world <developer.msw@gmail.com>
2022-07-25 13:17:30 +0900

    new: react-hook-form, mui input에 필요한 설정

---
+++ src/utils/input/index.ts
@@ -0,0 +1,73 @@
+import { NAME_VALIDATION, DOB_VALIDATION, PHONE_VALIDATION, EMAIL_VALIDATION } from './validation';
+
+export const SETTINGS = [
+  {
+    key: 'name',
+    title: '이름',
+    placeholder: '홍길동',
+    readOnly: false,
+    option: {
+      required: true,
+      pattern: {
+        value: NAME_VALIDATION,
+        message: '한글로 이름을 입력해주세요.',
+      },
+    },
+  },
+  {
+    key: 'dateOfBirth',
+    title: '생년월일',
+    placeholder: 'YYYY.MM.DD',
+    readOnly: false,
+    option: {
+      required: true,
+      pattern: {
+        value: DOB_VALIDATION,
+        message: 'YYYY.MM.DD 형식에 맞게 입력해주세요.',
+      },
+    },
+  },
+  {
+    key: 'address',
+    title: '거주지역',
+    placeholder: '거주지역 선택',
+    readOnly: true,
+    option: {
+      required: true,
+      pattern: undefined,
+    },
+  },
+  {
+    key: 'phone',
+    title: '연락처',
+    placeholder: '"-" 없이 입력해 주세요',
+    readOnly: false,
+    option: {
+      required: true,
+      pattern: {
+        value: PHONE_VALIDATION,
+        message: '"-" 없이 11자리 숫자만 입력해주세요.',
+      },
+    },
+  },
+  {
+    key: 'email',
+    title: '이메일',
+    placeholder: 'abc@abc.com',
+    readOnly: false,
+    option: {
+      required: true,
+      pattern: {
+        value: EMAIL_VALIDATION,
+        message: '"@", ".com"을 필수로 입력해주세요.',
+      },
+    },
+  },
+] as const;
+
+export const GENDER = {
+  KEY: 'gender',
+  TITLE: '성별',
+  FEMALE: '여자',
+  MALE: '남자',
+} as const;
