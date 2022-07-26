e5449fd8de0e74b04791bb3e0e252a25cf555520
corgi-world <developer.msw@gmail.com>
2022-07-26 14:48:00 +0900

    fix: index.tsx에 GlobalStyle 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from './styles/globals';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>,
