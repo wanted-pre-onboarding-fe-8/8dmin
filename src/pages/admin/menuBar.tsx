f59462dde2d65b099eb1011a4ed9a1e0d4fcde61
salangdung-i <dladmswl92@naver.com>
2022-07-26 16:45:19 +0900

    feact: active 상태에 따른 상세검색 컴포넌트 처리

--- src/pages/admin/menuBar.tsx
+++ src/pages/admin/menuBar.tsx
@@ -3,16 +3,24 @@ import styled from 'styled-components';
 import Button from '@mui/material/Button';
 import Search from './search';
 import DownloadLink from '../../components/DownloadLink';
+import DetailSearch from './detailSearch';
 
 export default function MenuBar() {
+  const [active, setActive] = React.useState(false);
+
+  const onActiveChange = () => {
+    setActive(!active);
+  };
+
   return (
     <>
       <Wrapper>
-        <Search />
+        <Search onActiveChange={onActiveChange} active={active} />
         <Button variant='contained' sx={{ p: '2px 40px', borderRadius: 4 }}>
           <DownloadLink />
         </Button>
       </Wrapper>
+      {active && <DetailSearch />}
     </>
   );
 }
