cc60b0605b4904b6dbbd7a5b0415a8cb402075e2
corgi-world <developer.msw@gmail.com>
2022-07-26 14:47:03 +0900

    refactor: 전역 스타일에 div, h1, p 태그 추가

--- src/styles/globals.js
+++ src/styles/globals.js
@@ -8,6 +8,18 @@ const GlobalStyle = createGlobalStyle`
     font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
   }
 
+  div {
+    box-sizing: border-box;
+  }
+  
+  h1, p {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+  }
+
   ul {
     list-style: none;
   }
