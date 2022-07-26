8beee24eb2bd6d17b35217d66c28784e337ab247
ohtaekwon <ohtk1119@gmail.com>
2022-07-26 21:30:40 +0900

    chore: index파일에 recoil데이터 연결

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,17 @@
-export default '';
+import { allApplicantState, seriesState, keywordState, selectState, pageState } from './atoms';
+import { applicantSelector } from './applicantSelector';
+import searchSelector from './searchSelector';
+import { sortApplicantSelector } from './sortApplicantSelector';
+import pageNationSelector from './pageNationSelector';
+
+export {
+  allApplicantState,
+  keywordState,
+  seriesState,
+  selectState,
+  pageState,
+  applicantSelector,
+  sortApplicantSelector,
+  searchSelector,
+  pageNationSelector,
+};
