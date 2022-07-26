6ed25bf71806e9cb8c9a1136116c63ed68517e89
ohtaekwon <ohtk1119@gmail.com>
2022-07-26 21:24:11 +0900

    feat: atom들만 모아둔 파일 생성

---
+++ src/store/atoms.ts
@@ -0,0 +1,25 @@
+import { atom } from 'recoil';
+
+export const allApplicantState = atom({
+  key: 'allapplicantState',
+  default: [],
+});
+
+export const seriesState = atom({
+  key: 'seriesState',
+  default: 1,
+});
+
+export const keywordState = atom({
+  key: 'keywordState',
+  default: '',
+});
+
+export const selectState = atom({
+  key: 'selectState',
+  default: 'dateOfApply',
+});
+export const pageState = atom({
+  key: 'pageState',
+  default: 1,
+});
