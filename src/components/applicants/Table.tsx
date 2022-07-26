4f56fe8332d8d04f519114251778c30bdd925059
Aum Il-Kyeong <sunaerocket@gmail.com>
2022-07-26 09:52:18 +0900

    refactor: 지원자 현황 테이블 헤드 분리

--- src/components/applicants/Table.tsx
+++ src/components/applicants/Table.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import MuiTable from '@mui/material/Table';
 import MuiTableBody from '@mui/material/TableBody';
-import MuiTableCell from '@mui/material/TableCell';
 import MuiTableContainer from '@mui/material/TableContainer';
-import MuiTableHead from '@mui/material/TableHead';
-import MuiTableRow from '@mui/material/TableRow';
 import MuiPaper from '@mui/material/Paper';
-import { TABLE_HEAD_LABELS } from '../../utils/constants/table';
 import TableRow from './TableRow';
+import TableHead from './TableHead';
 
 interface MockApplicant {
   id: number;
@@ -31,17 +28,7 @@ function Table({ applicants }: TableProps) {
   return (
     <MuiTableContainer component={MuiPaper}>
       <MuiTable sx={{ minWidth: 650 }} aria-label='applicants status table'>
-        <MuiTableHead>
-          <MuiTableRow>
-            {TABLE_HEAD_LABELS.map((label, index) => {
-              return (
-                <MuiTableCell key={index} align='center'>
-                  {label}
-                </MuiTableCell>
-              );
-            })}
-          </MuiTableRow>
-        </MuiTableHead>
+        <TableHead />
         <MuiTableBody>
           {applicants.map((applicant) => {
             return <TableRow key={applicant.id} applicant={applicant} />;
