631de33e842f45540b602e91cabceee692ed484b
ChuYeonBin <cndusqls98@gmail.com>
2022-07-27 00:21:41 +0900

    feat: 랜딩페이지 구현

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
-function Landing() {
-  return <div>Landing</div>;
+export default function Landing() {
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <ButtonWrapper>
+        <Button sx={{ p: 4 }} size='large' onClick={() => navigate('/apply')}>
+          AI 학습용 데이터 크라우드 워커 지원하러가기
+        </Button>
+        <Button sx={{ p: 4 }} size='large' onClick={() => navigate('/login')}>
+          관리자 로그인
+        </Button>
+      </ButtonWrapper>
+    </Wrapper>
+  );
 }
 
-export default Landing;
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ddd;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  background: white;
+  border-radius: 10px;
+`;
