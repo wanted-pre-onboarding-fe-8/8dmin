fe4ecd7e32d996775380175543737c9bc9070e28
ohtaekwon <ohtk1119@gmail.com>
2022-07-26 21:23:06 +0900

    feat: RecoilRoot연결

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/globals';
 import App from './App';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <GlobalStyle />
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   </React.StrictMode>,
 );
