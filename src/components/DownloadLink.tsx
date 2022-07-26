42eaeb35cc5cdacbc8f78a3be7fde53f18aeaf11
ChuYeonBin <cndusqls98@gmail.com>
2022-07-26 15:23:36 +0900

    fix: 다운로드 확인 창 제거

--- src/components/DownloadLink.tsx
+++ src/components/DownloadLink.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { FILE_NAME, TABLE_HEAD_LABELS } from '../utils/constants/table';
 
@@ -58,12 +58,7 @@ function DownloadLink() {
     },
   ];
 
-  const handleClick = (event: MouseEvent) => {
-    if (!confirm('엑셀 다운로드를 하시겠습니까?')) {
-      event.preventDefault();
-      return;
-    }
-
+  const handleClick = () => {
     let csvString = TABLE_HEAD_LABELS.join(',') + '\n';
 
     data.forEach((value) => {
