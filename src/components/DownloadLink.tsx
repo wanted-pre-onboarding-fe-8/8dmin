4889a4a57c406a49bf2eae5f3cd2077e582efc30
ChuYeonBin <cndusqls98@gmail.com>
2022-07-27 13:49:08 +0900

    fix: DownloadLink 컴포넌트 props type 수정

--- src/components/DownloadLink.tsx
+++ src/components/DownloadLink.tsx
@@ -1,70 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
+import { MockCandidates } from '../store/types';
 import { FILE_NAME, TABLE_HEAD_LABELS } from '../utils/constants/table';
 
-// interface DownloadLinkProps {
-//   data: {
-//     num: number;
-//     date: string;
-//     name: string;
-//     sex: '남' | '여';
-//     birth: string;
-//     phone: string;
-//     email: string;
-//     transport: string[];
-//     residence: string;
-//     status: boolean;
-//   }[];
-// }
+interface DownloadLinkProps {
+  applicant: MockCandidates;
+}
 
-function DownloadLink() {
+function DownloadLink({ applicant }: DownloadLinkProps) {
   const linkRef = React.useRef<HTMLAnchorElement>(null);
-  const data = [
-    {
-      num: 1,
-      date: '2022-07-01',
-      name: '추연빈',
-      sex: '남',
-      birth: '2222-11-11',
-      phone: '010-1234-5678',
-      email: 'cndusqls98@naver.com',
-      transport: ['버스'],
-      residence: '경기도',
-      status: true,
-    },
-    {
-      num: 2,
-      date: '2022-07-01',
-      name: '추연빈',
-      sex: '남',
-      birth: '2222-11-11',
-      phone: '010-1234-5678',
-      email: 'cndusqls98@naver.com',
-      transport: ['버스'],
-      residence: '경기도',
-      status: true,
-    },
-    {
-      num: 3,
-      date: '2022-07-01',
-      name: '추연빈',
-      sex: '남',
-      birth: '2222-11-11',
-      phone: '010-1234-5678',
-      email: 'cndusqls98@naver.com',
-      transport: ['버스', '택시', '자동차'],
-      residence: '경기도',
-      status: true,
-    },
-  ];
 
   const handleClick = () => {
     let csvString = TABLE_HEAD_LABELS.join(',') + '\n';
 
-    data.forEach((value) => {
-      csvString += `${value.num},${value.date},${value.name},${value.sex},${value.birth},${
-        value.phone
-      },${value.email},${value.transport.join(' ')},${value.residence},${value.status}\r\n`;
+    applicant.forEach((value) => {
+      csvString += `${value.id},${value.appliedAt},${value.name},${value.gender},${
+        value.dateOfBirth
+      },${value.phone},${value.email},${value.transportation.join(' ')},${value.region},${
+        value.accepted
+      }\r\n`;
 
       const blob = new Blob(['\uFEFF' + csvString], { type: 'text/csv;charset=utf-8' });
       const url = URL.createObjectURL(blob);
