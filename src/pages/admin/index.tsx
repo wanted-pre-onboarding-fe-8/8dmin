09916e5e8df435a2f6d9966f3f7b8088fedbcb64
corgi-world <developer.msw@gmail.com>
2022-07-27 15:58:44 +0900

    feat: 갱신 시각 표시하는 버튼 구현

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import MenuBar from './menuBar';
 import Status from '../../components/applicants/Status';
@@ -17,6 +17,9 @@ import {
   pageNationSelector,
 } from '../../store';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+import { format } from 'date-fns';
+import RefreshIcon from '@mui/icons-material/Refresh';
 
 const {
   ORDER_ID,
@@ -77,11 +80,23 @@ export default function Admin() {
   }, []);
 
   useEffect(() => {
-    setApplicants(mockdata.applicants);
+    renewal();
   }, []);
+
+  const [renewalDate, setRenewalDate] = useState('');
+  const renewal = () => {
+    setApplicants(mockdata.applicants);
+    const nowTime = getRenewalDate();
+    setRenewalDate(nowTime);
+  };
+
   return (
     <Wrapper>
       <Header>AI 학습용 교통 데이터 수집을 위한 클라우드 워커 지원 현황</Header>
+      <RenewalWrapper onClick={() => renewal()}>
+        <RenewalTimeText>{renewalDate}</RenewalTimeText>
+        <RenewalButton>hello</RenewalButton>
+      </RenewalWrapper>
       <MenuBar />
       <Status />
     </Wrapper>
@@ -89,6 +104,7 @@ export default function Admin() {
 }
 
 const Wrapper = styled.div`
+  position: relative;
   padding: 10px 30px;
   margin-top: 65px;
   flex-grow: 1;
@@ -99,4 +115,28 @@ const Header = styled.header`
   font-weight: 700;
   font-size: 24px;
   margin-top: 30px;
+`;
+
+function getRenewalDate() {
+  const now = new Date();
+  return 'new : ' + format(now, 'hh시 mm분 ss초');
+}
+const RenewalWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: 33px;
+  right: 30px;
+  background-color: #1975d2;
+  padding: 10px 15px 10px 15px;
+  border-radius: 15px;
+  cursor: pointer;
+`;
+const RenewalTimeText = styled.p`
+  color: white;
+  margin-right: 10px;
+  font-size: 14px;
+`;
+const RenewalButton = styled(RefreshIcon)`
+  color: white;
 `;
